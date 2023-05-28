16502553b14a18637cb6524594a2fe093359b551
malgumi <malgumizzang@naver.com>
2023-05-28 23:24:37 +0900

    firebase 수정

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,46 @@ import 'package:capstone/screens/subject/MSmain.dart';
 import 'package:capstone/screens/post/party_board.dart' as PartyBoard;
 import 'package:capstone/screens/post/free_board.dart' as FreeBoard;
 import 'package:capstone/screens/post/QnA_board.dart' as QABoard;
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 
 
 void main() async {
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   WidgetsFlutterBinding.ensureInitialized();
+
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //======↓IOS용 권한 허용 코드↓=========
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+  //======↑IOS용 권한 허용 코드↑=========
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
