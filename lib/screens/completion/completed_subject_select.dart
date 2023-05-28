169370dfea5c52f0ebfdd2bd80cee22c95177066
sninyoung <tlsdud4325@gmail.com>
2023-05-28 19:45:19 +0900

    선택 페이지 _completionSelectState FutureBuilder사용

--- lib/screens/completion/completed_subject_select.dart
+++ lib/screens/completion/completed_subject_select.dart
@@ -6,8 +6,8 @@ import 'package:multi_select_flutter/multi_select_flutter.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:capstone/screens/completion/completion_status.dart';
 
-//이수과목 선택 페이지
 
+//이수과목 선택 페이지
 void main() {
   runApp(MaterialApp(
     title: '나의 이수현황',
@@ -28,7 +28,6 @@ Future<List> fetchSubjects() async {
 }
 
 //전공기초 전공선택 분류
-
 List<Subject> compulsorySubjects = [];
 List<Subject> electiveSubjects = [];
 
@@ -74,75 +73,84 @@ Future<bool> postRequiredCourses(String studentId, List<String> courses) async {
 //
 class CompletionSelect extends StatefulWidget {
   CompletionSelect({Key? key}) : super(key: key);
-
   @override
   _CompletionSelectState createState() => _CompletionSelectState();
 }
 
 class _CompletionSelectState extends State<CompletionSelect> {
-  List<Subject> _compulsorySelections = [];
-  List<Subject> _electiveSelections = [];
+  List<Subject?> _compulsorySelections = [];
+  List<Subject?> _electiveSelections = [];
 
   String get studentId => '';
   List<String> courses = [];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        MultiSelectChipField<Subject>(
-          items: compulsorySubjects
-              .map((subject) =>
-                  MultiSelectItem<Subject>(subject, subject.subject_name))
-              .toList(),
-          title: Text("전공기초과목"),
-          selectedChipColor: Colors.blue,
-          onTap: (values) {
-            setState(() {
-              _compulsorySelections = values;
-            });
-          },
-        ),
-        MultiSelectChipField<Subject>(
-          items: electiveSubjects
-              .map((subject) =>
-                  MultiSelectItem<Subject>(subject, subject.subject_name))
-              .toList(),
-          title: Text("전공선택과목"),
-          selectedChipColor: Colors.blue,
-          onTap: (values) {
-            setState(() {
-              _electiveSelections = values;
-            });
-          },
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            // 선택한 과목들을 courses 변수에 할당
-            courses.clear(); // 기존의 선택한 과목들을 초기화
-            courses.addAll(_compulsorySelections.map((subject) => subject.subject_name));
-            courses.addAll(_electiveSelections.map((subject) => subject.subject_name));
+    return FutureBuilder<List>(
+      future: fetchSubjects(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          divideSubjects(snapshot.data!);
 
-            bool success = await postRequiredCourses(studentId, courses);
-            if (success) {
-              // 성공적으로 이수과목 정보가 저장되었을 경우 알림
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('Subjects successfully saved!')),
-              );
-              // Navigator.push(context, MaterialPageRoute(builder: (context) => CompletionStatusPage()),
-              // );
-            } else {
-              // 요청이 실패했을 경우 에러 메시지 표시
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('Failed to save subjects.')),
-              );
-            }
-          },
-          child: Text('저장'),
-        )
-,
+          return Column(
+            children: [
+              MultiSelectChipField<Subject>(
+                items: compulsorySubjects
+                    .map((subject) =>
+                    MultiSelectItem<Subject>(subject, subject.subject_name))
+                    .toList(),
+                title: Text("전공기초과목"),
+                selectedChipColor: Colors.blue,
+                onTap: (values) {
+                  setState(() {
+                    _compulsorySelections = values;
+                  });
+                },
+              ),
+              MultiSelectChipField<Subject>(
+                items: electiveSubjects
+                    .map((subject) =>
+                    MultiSelectItem<Subject>(subject, subject.subject_name))
+                    .toList(),
+                title: Text("전공선택과목"),
+                selectedChipColor: Colors.blue,
+                onTap: (values) {
+                  setState(() {
+                    _electiveSelections = values;
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // 선택한 과목들을 courses 변수에 할당
+                  courses.clear(); // 기존의 선택한 과목들을 초기화
+                  courses.addAll(_compulsorySelections.map((subject) => subject?.subject_name).where((name) => name != null).cast<String>());
+                  courses.addAll(_electiveSelections.map((subject) => subject?.subject_name).where((name) => name != null).cast<String>());
 
-      ],
+                  bool success = await postRequiredCourses(studentId, courses);
+                  if (success) {
+                    // 성공적으로 이수과목 정보가 저장되었을 경우 알림
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Subjects successfully saved!')),
+                    );
+                    // Navigator.push(context, MaterialPageRoute(builder: (context) => CompletionStatusPage()),
+                    // );
+                  } else {
+                    // 요청이 실패했을 경우 에러 메시지 표시
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Failed to save subjects.')),
+                    );
+                  }
+                },
+                child: Text('저장'),
+              ),
+            ],
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        return CircularProgressIndicator(); // 데이터를 기다리는 동안에는 CircularProgressIndicator를 표시
+      },
     );
   }
 }
